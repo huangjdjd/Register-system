4a5bc1a5568136bfe635164a508bb6efe53bc361
huang <z0908565970@gmail.com>
2024-03-05 19:52:59 +0800

    successful

--- backend.py
+++ backend.py
@@ -106,4 +106,4 @@ def adminCheck():
     return js.dumps({'checkstatus':True})
     
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=5000)
+    app.run(debug=True,host='0.0.0.0',port=5050)
